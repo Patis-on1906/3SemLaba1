f2e50642105385b5178b3960e7a2d0b0a7b55a57
Patis-on1906 <vleontev979@gmail.com>
2025-10-02 12:49:16 +0700

    final

--- Laba1/Program.cs
+++ Laba1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Laba1;
 
 namespace Laba1
@@ -20,76 +17,105 @@ namespace Laba1
 
                 int input;
                 if (!int.TryParse(Console.ReadLine(), out input))
-                    continue;
-
-                switch (input)
                 {
-                    case 1:
-                        {
-                            Console.Write("Enter manufacturer: ");
-                            string manufacturer = Console.ReadLine();
-                            compTechList.Add(new ComputerTechnology(manufacturer));
-                            break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("Enter manufacturer, resolution (WIDTHxHEIGHT), screen size and refresh rate:");
-                            string manufacturer = Console.ReadLine();
+                    Console.WriteLine("Invalid input, enter a number 1-5.");
+                    continue;
+                }
 
-                            Console.Write("Resolution (e.g. 1920x1080): ");
-                            string resolution = Console.ReadLine();
-                            string[] parts = resolution.Split('x');
-                            int width = int.Parse(parts[0]);
-                            int height = int.Parse(parts[1]);
-
-                            Console.Write("Screen size: ");
-                            float screenSize = float.Parse(Console.ReadLine());
-
-                            Console.Write("Refresh rate: ");
-                            int refreshRate = int.Parse(Console.ReadLine());
-
-                            compTechList.Add(new MonitorTech(manufacturer, new List<int> { width, height }, screenSize, refreshRate));
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.WriteLine("Enter manufacturer, color, type of keys (membrane, mechanical or optical), connection type (wired or wireless) and whether there is a backlight (1 or 0):");
-
-                            Console.Write("Manufacturer: ");
-                            string manufacturer = Console.ReadLine();
-
-                            Console.Write("Color: ");
-                            string color = Console.ReadLine();
-
-                            Console.Write("Keys type: ");
-                            string keys = Console.ReadLine();
-
-                            Console.Write("Connection type: ");
-                            string connection = Console.ReadLine();
-
-                            Console.Write("Has backlight (1 or 0): ");
-                            bool hasBacklight = Console.ReadLine() == "1";
-
-                            compTechList.Add(new Keyboard(manufacturer, color, keys, connection, hasBacklight));
-                            break;
-                        }
-                    case 4:
-                        {
-                            int i = 1;
-                            foreach (var item in compTechList)
+                try
+                {
+                    switch (input)
+                    {
+                        case 1:
                             {
-                                Console.Write($"{i++}: ");
-                                item.Print();
+                                Console.Write("Enter manufacturer: ");
+                                string manufacturer = Console.ReadLine();
+                                compTechList.Add(new ComputerTechnology(manufacturer));
+                                break;
                             }
+                        case 2:
+                            {
+                                Console.WriteLine("Enter manufacturer, resolution (width, height), screen size and refresh rate:");
+                                Console.Write("Manufacturer: ");
+                                string manufacturer = Console.ReadLine();
+
+                                Console.Write("Width resolution: ");
+                                uint.TryParse(Console.ReadLine(), out uint widthResolution);
+
+                                Console.Write("Height resolution: ");
+                                uint.TryParse(Console.ReadLine(), out uint heigthResolution);
+
+                                Console.Write("Screen size: ");
+                                float.TryParse(Console.ReadLine(), out float screenSize);
+
+                                Console.Write("Refresh rate: ");
+                                int.TryParse(Console.ReadLine(), out int refreshRate);
+
+
+                                compTechList.Add(new MonitorTech(manufacturer, new List<uint> { widthResolution, heigthResolution }, screenSize, refreshRate));
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Enter manufacturer, color, type of keys (membrane, mechanical or optical), connection type (wired or wireless) and whether there is a backlight (1 or 0):");
+
+                                Console.Write("Manufacturer: ");
+                                string manufacturer = Console.ReadLine();
+
+                                Console.Write("Color: ");
+                                string color = Console.ReadLine();
+
+                                Console.Write("Keys type: ");
+                                string keys = Console.ReadLine();
+
+                                Console.Write("Connection type: ");
+                                string connection = Console.ReadLine();
+
+                                Console.Write("Has backlight (1 or 0): ");
+                                string inputBacklight = Console.ReadLine();
+
+                                if (inputBacklight != "1" && inputBacklight != "0")
+                                {
+                                    Console.WriteLine("Error: has backlight: enter only 1 or 0.");
+                                    continue;
+                                }
+
+                                bool hasBacklight = inputBacklight == "1";
+
+
+                                compTechList.Add(new Keyboard(manufacturer, color, keys, connection, hasBacklight));
+                                break;
+                            }
+                        case 4:
+                            {
+                                if (!compTechList.Any()) // если список пустой, то выводим сообщение об этом
+                                {
+                                    Console.WriteLine("The list is empty.");
+                                }
+                                int i = 1;
+                                foreach (var item in compTechList)
+                                {
+                                    Console.Write($"{i++}: ");
+                                    item.Print();
+                                }
+                                break;
+                            }
+                        case 5:
+                            return;
+                        default:
+                            Console.WriteLine("Choose 1-5.");
                             break;
-                        }
-                    case 5:
-                        return;
-                    default:
-                        break;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected error: " + ex.Message);
                 }
             }
         }
     }
 }
-
--- Laba1Tests/MonitorTechTest.cs
+++ Laba1Tests/MonitorTechTest.cs
@@ -8,11 +8,11 @@ namespace Laba1Tests
         [TestMethod]
         public void Constructor_ValidParameters_SetsAllProperties()
         {
-            var resolution = new List<int> { 1920, 1080 };
+            var resolution = new List<uint> { 1920, 1080 };
             var monitor = new MonitorTech("Samsung", resolution, 27.0f, 144);
 
             Assert.AreEqual("Samsung", monitor.Manufacturer);
-            CollectionAssert.AreEqual(new List<int> { 1920, 1080 }, monitor.Resolution);
+            CollectionAssert.AreEqual(new List<uint> { 1920, 1080 }, monitor.Resolution);
             Assert.AreEqual(27.0f, monitor.ScreenSize);
             Assert.AreEqual(144, monitor.RefreshRate);
         }
@@ -21,7 +21,7 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Constructor_InvalidResolutionCount_ThrowsException()
         {
-            var invalidResolution = new List<int> { 1920 };
+            var invalidResolution = new List<uint> { 1920 };
             var monitor = new MonitorTech("Samsung", invalidResolution, 27.0f, 144);
         }
 
@@ -29,7 +29,7 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Constructor_InvalidResolutionValues_ThrowsException()
         {
-            var invalidResolution = new List<int> { 0, 1080 };
+            var invalidResolution = new List<uint> { 0, 1080 };
             var monitor = new MonitorTech("Samsung", invalidResolution, 27.0f, 144);
         }
 
@@ -37,7 +37,7 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Constructor_InvalidScreenSize_ThrowsException()
         {
-            var resolution = new List<int> { 1920, 1080 };
+            var resolution = new List<uint> { 1920, 1080 };
             var monitor = new MonitorTech("Samsung", resolution, -1.0f, 144);
         }
 
@@ -45,15 +45,15 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Constructor_InvalidRefreshRate_ThrowsException()
         {
-            var resolution = new List<int> { 1920, 1080 };
+            var resolution = new List<uint> { 1920, 1080 };
             var monitor = new MonitorTech("Samsung", resolution, 27.0f, 0);
         }
 
         [TestMethod]
         public void Resolution_PropertySetter_ValidValue_SetsCorrectly()
         {
-            var monitor = new MonitorTech("LG", new List<int> { 1920, 1080 }, 24.0f, 60);
-            var newResolution = new List<int> { 2560, 1440 };
+            var monitor = new MonitorTech("LG", new List<uint> { 1920, 1080 }, 24.0f, 60);
+            var newResolution = new List<uint> { 2560, 1440 };
             monitor.Resolution = newResolution;
 
             CollectionAssert.AreEqual(newResolution, monitor.Resolution);
@@ -63,40 +63,32 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Resolution_PropertySetter_InvalidValue_ThrowsException()
         {
-            var monitor = new MonitorTech("LG", new List<int> { 1920, 1080 }, 24.0f, 60);
-            var invalidResolution = new List<int> { 2560, 1440, 2160 };
+            var monitor = new MonitorTech("LG", new List<uint> { 1920, 1080 }, 24.0f, 60);
+            var invalidResolution = new List<uint> { 2560, 1440, 2160 };
             monitor.Resolution = invalidResolution;
         }
 
         [TestMethod]
         public void SetResolution_ValidParameters_SetsCorrectly()
         {
-            var monitor = new MonitorTech("Dell", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("Dell", new List<uint> { 1920, 1080 }, 24.0f, 60);
             monitor.SetResolution(3840, 2160);
 
-            CollectionAssert.AreEqual(new List<int> { 3840, 2160 }, monitor.Resolution);
+            CollectionAssert.AreEqual(new List<uint> { 3840, 2160 }, monitor.Resolution);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void SetResolution_InvalidWidth_ThrowsException()
         {
-            var monitor = new MonitorTech("Dell", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("Dell", new List<uint> { 1920, 1080 }, 24.0f, 60);
             monitor.SetResolution(0, 1080);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void SetResolution_InvalidHeight_ThrowsException()
-        {
-            var monitor = new MonitorTech("Dell", new List<int> { 1920, 1080 }, 24.0f, 60);
-            monitor.SetResolution(1920, -1);
         }
 
         [TestMethod]
         public void ScreenSize_PropertySetter_ValidValue_SetsCorrectly()
         {
-            var monitor = new MonitorTech("ASUS", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("ASUS", new List<uint> { 1920, 1080 }, 24.0f, 60);
             monitor.ScreenSize = 32.5f;
 
             Assert.AreEqual(32.5f, monitor.ScreenSize);
@@ -106,14 +98,14 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void ScreenSize_PropertySetter_InvalidValue_ThrowsException()
         {
-            var monitor = new MonitorTech("ASUS", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("ASUS", new List<uint> { 1920, 1080 }, 24.0f, 60);
             monitor.ScreenSize = 0f;
         }
 
         [TestMethod]
         public void RefreshRate_PropertySetter_ValidValue_SetsCorrectly()
         {
-            var monitor = new MonitorTech("Acer", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("Acer", new List<uint> { 1920, 1080 }, 24.0f, 60);
             monitor.RefreshRate = 240;
 
             Assert.AreEqual(240, monitor.RefreshRate);
@@ -123,7 +115,7 @@ namespace Laba1Tests
         [ExpectedException(typeof(ArgumentException))]
         public void RefreshRate_PropertySetter_InvalidValue_ThrowsException()
         {
-            var monitor = new MonitorTech("Acer", new List<int> { 1920, 1080 }, 24.0f, 60);
+            var monitor = new MonitorTech("Acer", new List<uint> { 1920, 1080 }, 24.0f, 60);
 
             monitor.RefreshRate = 0;
         }
@@ -131,13 +123,13 @@ namespace Laba1Tests
         [TestMethod]
         public void Resolution_Property_ReturnsCopyNotReference()
         {
-            var originalResolution = new List<int> { 1920, 1080 };
+            var originalResolution = new List<uint> { 1920, 1080 };
             var monitor = new MonitorTech("Test", originalResolution, 24.0f, 60);
 
             var retrievedResolution = monitor.Resolution;
             retrievedResolution[0] = 9999;
 
-            Assert.AreEqual(1920, monitor.Resolution[0]);
+            Assert.AreEqual(1920u, monitor.Resolution[0]);
         }
     }
 }
